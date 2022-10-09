29ac4cee4a152789ea47625a56398deb9de6f6d9
jeansillva <jeancarlos_bh@outlook.com>
2022-10-09 12:51:49 -0300

    first commit

--- src/App.java
+++ src/App.java
@@ -7,8 +7,8 @@ public class App {
         System.out.print("Entre com o raio do circulo: ");
         double raio = sc.nextDouble();
         sc.close();
-        // Requisito 2: Calcular a área do circulo
-        // =PI() * POTÊNCIA(C3;2)
+        //Requisito 2: Calcular a área do circulo
+        //PI() * POTÊNCIA(C3;2)
         double area = Math.PI * Math.pow(raio, 2);
         System.out.println("A área do circulo de raio " + raio + " é igual a " + area);
     }
